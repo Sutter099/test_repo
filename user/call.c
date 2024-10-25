9146ca9c652a4e13e9b2736ee288a06b93c53db1
Ze Huang <18771902331@163.com>
2024-10-25 16:27:36 +0800

    lab1: RISC-V assembly

--- user/call.c
+++ user/call.c
@@ -12,6 +12,12 @@ int f(int x) {
 }
 
 void main(void) {
+	unsigned int i = 0x00646c72;
+	printf("H%x Wo%s", 57616, &i);
+
+  asm volatile("li a2, 0xaa");
+  printf("x=%d y=%d\n", 3);
+
   printf("%d %d\n", f(8)+1, 13);
   exit(0);
 }
